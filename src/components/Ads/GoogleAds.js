fbde6197a1abb481bc049d78c2c0cca103403eab
o-x-y-g-e-n <shivamgohel777@gmail.com>
2022-05-05 21:32:07 -0400

    new changes for ads

--- src/components/Ads/GoogleAds.js
+++ src/components/Ads/GoogleAds.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AdSense from 'react-adsense';
 
 const GoogleAds = ({ layout = "image", slot }) => {
   return (
     <AdSense.Google
       client="ca-pub-6122504498304460"
-      slot="7806394673"
-      style={{ display: "block" }}
+      slot="1981420027"
+      style={{ display: "block",textAlign: "center"}}
       format="auto"
       responsive="true"
       layoutKey="-gw-1+2a-9x+5c"
